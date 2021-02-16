1d436c8f7aaad9018af1d0334280bb107a108838
PandhuWibowo <pandhuw58@gmail.com>
2021-02-16 23:34:28 +0700

    Create auth login

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomersController;
-
+use App\Http\Controllers\SignInController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,6 +15,11 @@ use App\Http\Controllers\CustomersController;
 |
 */
 
+//Access-Control-Allow-Origin: *
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -25,4 +30,7 @@ Route::prefix('v1')->group(function () {
     Route::get('/customers/{id}', [CustomersController::class, 'show']);
     Route::put('/customers/{id}', [CustomersController::class, 'update']);
     Route::put('/customers/{id}/status', [CustomersController::class, 'updateStatus']);
+
+    Route::post('/sign-in/authenticate', [SignInController::class, 'authenticate']);
+    Route::get('/sign-in/authenticate', [SignInController::class, 'authenticate'])->name('login');
 });
