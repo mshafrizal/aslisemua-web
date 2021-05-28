108d4dc8ab4cb6c88bd29f362ec59960164413c3
Kh(Alif)a Visi Islami <khalifavi@gmail.com>
2021-05-28 21:56:51 +0700

    fix(seeder): category seeder error fix

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -19,6 +19,7 @@ class CategorySeeder extends Seeder
             [
                 'id' => '298db7d4-826d-46ee-ab4b-c7fa5edda7bc',
                 'name' => 'Bags',
+                'slug' => 'bags',
                 'description' => 'Bags',
                 'file_path' => null,
                 'parent' => '',
@@ -31,6 +32,7 @@ class CategorySeeder extends Seeder
             [
                 'id' => '51fce767-e3e5-4321-8694-30c79623b1bc',
                 'name' => 'Clothes',
+                'slug' => 'clothes',
                 'description' => 'Clothes',
                 'file_path' => null,
                 'parent' => '',
@@ -43,6 +45,7 @@ class CategorySeeder extends Seeder
             [
                 'id' => '348c8cbe-b200-4135-bdcc-c1de0b4530fa',
                 'name' => 'Furniture',
+                'slug' => 'furniture',
                 'description' => 'Furniture',
                 'file_path' => null,
                 'parent' => '',
@@ -55,6 +58,7 @@ class CategorySeeder extends Seeder
             [
                 'id' => '54ce287f-c357-491c-a933-67c87243616c',
                 'name' => 'Jewelry',
+                'slug' => 'jewelry',
                 'description' => 'Jewelry',
                 'file_path' => null,
                 'parent' => '',
