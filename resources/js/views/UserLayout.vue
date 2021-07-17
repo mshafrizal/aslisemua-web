5d1faf59787b7f269e6371f9888ad406bb411e5c
Muhammad Shafrizal <mshafrizal@gmail.com>
2021-07-17 09:30:39 +0700

    Update navigation bar desktop view

--- resources/js/views/UserLayout.vue
+++ resources/js/views/UserLayout.vue
@@ -3,20 +3,8 @@
     <v-snackbar top v-if="snackbar" v-model="snackbar.value" :color="snackbar.type">
       {{snackbar ? snackbar.message : '-'}}
     </v-snackbar>
-
-    <v-container v-if="$route.meta.navbar">
-      <v-row>
-        <v-col></v-col>
-        <v-col>
-          <h1 class="text-center logo">ASLISEMUA</h1>
-        </v-col>
-        <v-col class="d-flex justify-end">
-          <v-btn icon><v-icon>mdi-heart-outline</v-icon></v-btn>
-          <v-btn icon><v-icon>mdi-cart-outline</v-icon></v-btn>
-          <v-btn icon><v-icon>mdi-account-outline</v-icon></v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
+    <user-navigation />
+    <v-divider />
     <v-main>
       <router-view />
     </v-main>
@@ -25,8 +13,10 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import UserNavigation from "../components/shared/UserNavigation";
 export default {
   name: "UserLayout",
+  components: {UserNavigation},
   computed: {
     ...mapGetters([
       'getSnackbar'
@@ -39,7 +29,4 @@ export default {
 </script>
 
 <style scoped>
-  .logo {
-    font-size: 2.5rem;
-  }
 </style>
