5dd06e0a84f382e41be5ab44bab832eddb03931a
Muhammad Shafrizal <mshafrizal@gmail.com>
2021-07-26 01:12:07 +0700

    Install vue-awesome-swiper

---
+++ resources/js/components/shared/ProductItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="product">
+    <img src="/images/product-1.jpg" alt="" class="product__img">
+    <div class="product__info">
+      <h1 class="text-uppercase font-weight-bold text-lg black--text">NIKE</h1>
+      <p class="black--text mt-0 mb-1">Air Jordan</p>
+      <p class="black--text mt-0 mb-1">Size: 42</p>
+      <p class="text-yellow-500 mt-0 mb-1">Rp 3.250.000</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductItem",
+  props: {
+    product: {
+      type: Object,
+      default: null,
+      required: true
+    }
+  },
+  mounted() {
+    // console.log(this.product)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
