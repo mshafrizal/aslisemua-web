25b332631a7970499a39a346568c5a68efc18933
pandhuwibowo <pandhuw58@gmail.com>
2021-08-07 14:54:07 +0700

    Create API of Customer Address

--- app/Models/CustomerAddress.php
+++ app/Models/CustomerAddress.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Customer;
 
 class CustomerAddress extends Model
 {
@@ -12,6 +13,7 @@ class CustomerAddress extends Model
     protected $table = 'customer_addresses';
     protected $primaryKey = 'id';
     public $incrementing = false;
+    public $timestamps = false;
     protected $fillable = [
         'id',
         'customer_id',
@@ -20,4 +22,14 @@ class CustomerAddress extends Model
         'is_default',
         'address'
     ];
+
+    /**
+     * Get the customer that owns the CustomerAddress
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
