f1bd281945c3d5cec726fc075fd1121342f5bbbf
mshafrizal <mshafrizal@gmail.com>
2021-08-07 22:45:44 +0700

    Fix API url

--- resources/js/store/modules/customer.js
+++ resources/js/store/modules/customer.js
@@ -2,22 +2,22 @@ const state = () => ({})
 const getters = {}
 const actions = {
   fetchCustomers (context, payload) {
-    return axios.get(`${process.env.MIX_APP_URL}/api/v1/customers`).then(response => {
+    return axios.get(`/api/v1/customers`).then(response => {
       return response.data
     }).catch(error => error)
   },
   fetchCustomer (context, payload) {
-    return axios.get(`${process.env.MIX_APP_URL}/api/v1/customers/${payload.customer_id}`).then(response => {
+    return axios.get(`/api/v1/customers/${payload.customer_id}`).then(response => {
       return response.data
     }).catch(error => error)
   },
   updateCustomer (context, payload) {
-    return axios.get(`${process.env.MIX_APP_URL}/api/v1/customers/${payload.customer_id}`).then(response => {
+    return axios.get(`/api/v1/customers/${payload.customer_id}`).then(response => {
       return response.data
     }).catch(error => error)
   },
   deleteCustomer (context, payload) {
-    return axios.delete(`${process.env.MIX_APP_URL}/api/v1/customers/${payload.customer_id}`).then(response => {
+    return axios.delete(`/api/v1/customers/${payload.customer_id}`).then(response => {
       return response.data
     }).catch(error => error)
   }
