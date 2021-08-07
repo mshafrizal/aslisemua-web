675e6127c87fe5e741c48984f46d87a6a0a60ba8
mshafrizal <mshafrizal@gmail.com>
2021-08-07 22:42:36 +0700

    Add swiper and configure tiptapvuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,14 +4,14 @@ window.Vue = require('vue').default;
 import Vuetify from 'vuetify';
 import Vuex from 'vuex';
 import VCurrencyField from "v-currency-field";
-import { TiptapVuetifyPlugin } from 'tiptap-vuetify'
-// don't forget to import CSS styles
-import 'tiptap-vuetify/dist/main.css'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/css/swiper.min.css'
 // Vuetify's CSS styles
 import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css'
+import { TiptapVuetifyPlugin } from 'tiptap-vuetify'
+// don't forget to import CSS styles
+import 'tiptap-vuetify/dist/main.css'
 const vuetify = new Vuetify()
 
 Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
@@ -28,7 +28,7 @@ Vue.use(VCurrencyField, {
 })
 Vue.use(Vuex);
 Vue.use(TiptapVuetifyPlugin, {
-  vuetify, iconsGroup: 'md'
+  vuetify, iconsGroup: 'mdi'
 })
 Vue.prototype.$axios = axios
 
