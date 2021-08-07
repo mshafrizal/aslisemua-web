1d9e24e32ad0f1141c39a893456f74800febd3be
mshafrizal <mshafrizal@gmail.com>
2021-08-07 22:38:30 +0700

    Add is_navbar payload

--- resources/js/store/modules/category.js
+++ resources/js/store/modules/category.js
@@ -2,42 +2,42 @@ const state = () => ({})
 const getters = {}
 const actions = {
   adminCreateCategory (context, payload) {
-    return axios.post(`${process.env.MIX_APP_URL}/api/v1/categories/private`, payload).then(response => {
+    return axios.post(`/api/v1/categories/private`, payload).then(response => {
       return response.data
     }).catch(error => error)
   },
   adminFetchCategories (context, payload) {
-    return axios.get(`${process.env.MIX_APP_URL}/api/v1/categories/private`, payload).then(response => {
+    return axios.get(`/api/v1/categories/private`, payload).then(response => {
       return response.data
     }).catch(error => error)
   },
   adminFetchCategory (context, payload) {
-    return axios.get(`${process.env.MIX_APP_URL}/api/v1/categories/private/${payload.category_id}`).then(response => {
+    return axios.get(`/api/v1/categories/private/${payload.category_id}`).then(response => {
       return response.data
     }).catch(error => error)
   },
   adminUpdateCategory (context, payload) {
-    return axios.post(`${process.env.MIX_APP_URL}/api/v1/categories/private/update/${payload.category_id}`, payload.data).then(response => {
+    return axios.post(`/api/v1/categories/private/update/${payload.category_id}`, payload.data).then(response => {
       return response.data
     }).catch(error => error)
   },
   adminUpdateCategoryStatus (context, payload) {
-    return axios.post(`${process.env.MIX_APP_URL}/api/v1/categories/private/update/status/${payload.category_id}`).then(response => {
+    return axios.post(`/api/v1/categories/private/update/status/${payload.category_id}`).then(response => {
       return response.data
     }).catch(error => error)
   },
   adminDeleteCategory (context, payload) {
-    return axios.post(`${process.env.MIX_APP_URL}/api/v1/categories/private/delete/${payload.category_id}`).then(response => {
+    return axios.post(`/api/v1/categories/private/delete/${payload.category_id}`).then(response => {
       return response.data
     }).catch(error => error)
   },
   adminBulkDeleteCategories (context, payload) {
-    return axios.post(`${process.env.MIX_APP_URL}/api/v1/categories/private/delete/bulk`, payload).then(response => {
+    return axios.post(`/api/v1/categories/private/delete/bulk`, payload).then(response => {
       return response.data
     }).catch(error => error)
   },
   fetchCategories (context, query) {
-    return axios.get(`${process.env.MIX_APP_URL}/api/v1/categories/public/${query}`).then(response => {
+    return axios.get(`/api/v1/categories/public/${query}`).then(response => {
       return response.data
     }).catch(error => error)
   },
