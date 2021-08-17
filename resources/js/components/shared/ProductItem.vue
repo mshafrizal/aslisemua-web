78ac64027b8a0305732d20531e5b015f7b27c8f8
Muhammad Shafrizal <mshafrizal@gmail.com>
2021-08-18 06:40:40 +0700

    Update product item for demo

--- resources/js/components/shared/ProductItem.vue
+++ resources/js/components/shared/ProductItem.vue
@@ -1,6 +1,7 @@
 <template>
   <router-link :to="`/product/${product.slug}`" class="product" v-if="product && product.products_images.length > 0">
-    <img :src="resolveImagePath(product.products_images[0].image_path)" alt="" class="product__img">
+<!--    <img :src="resolveImagePath(product.products_images[0].image_path)" alt="" class="product__img">-->
+    <img :src="product.products_images[0].image_path" alt="" class="product__img">
     <div class="product__info">
       <h1 class="text-uppercase font-weight-bold text-lg black--text">{{product.brand.name}}</h1>
       <p class="black--text mt-0 mb-1">{{product.name}}</p>
