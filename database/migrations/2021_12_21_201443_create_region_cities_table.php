e1d017773303d8a0b257b34d6ae816fa78398aaa
Muhammad Shafrizal <mshafrizal@gmail.com>
2021-12-21 20:20:25 +0700

    Add RegionCity Migration

---
+++ database/migrations/2021_12_21_201443_create_region_cities_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegionCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('region_cities', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('province_id');
+            $table->string('name', 100);
+            $table->timestamps();
+        });
+
+        Schema::table('region_cities', function (Blueprint $table) {
+          $table->foreign('province_id')->references('id')->on('region_provinces');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('region_cities');
+    }
+}
