86b938987c29a0023dda4a1ae5b6f0d0c6268d5a
mshafrizal <mshafrizal@gmail.com>
2022-02-12 17:24:46 +0700

    Add payment type file

---
+++ resources/js/views/admin/payment types/PaymentTypes.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row>
+    <v-col cols="12"><h2>Payment Type List</h2></v-col>
+    <v-col>
+        <v-data-table
+            :data="[]"
+            :headers="headers"
+        >
+        </v-data-table>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+    name: "PaymentTypes",
+    data: function() {
+        return {
+            headers: [
+                { text: "Name", value: "name" },
+                { text: "Key", value: "key_name" },
+                { text: "Actions", value: "" },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
