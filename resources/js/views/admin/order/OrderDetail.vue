0f3defb0495414d78be1f4a88f6559e9f51a0896
Muhammad Shafrizal <mshafrizal@gmail.com>
2022-04-12 01:54:50 +0700

    Revert to newest update

---
+++ resources/js/views/admin/order/OrderDetail.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-row>
+      <v-col cols="12">
+          <h2>Brands</h2>
+      </v-col>
+      <v-col cols="12">
+          <v-card>
+              <v-card-title>
+                  ORDER INFO
+              </v-card-title>
+              <v-divider></v-divider>
+              <v-card-text>
+                  <v-col cols="4">
+                      <strong>Order ID</strong>
+                  </v-col>
+                  <v-col cols="8">
+                      
+                  </v-col>
+              </v-card-text>
+          </v-card>
+      </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+    name: 'OrderDetail',
+    data: function () {
+        return {
+            order: null,
+            loading: true,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
