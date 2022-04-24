4eded928170fb7b92581e06b9093c3d9c697cc3c
Muhammad Shafrizal <mshafrizal@gmail.com>
2022-04-24 15:05:31 +0700

    Style fix user footer and consignment

--- resources/js/components/shared/UserFooter.vue
+++ resources/js/components/shared/UserFooter.vue
@@ -6,7 +6,7 @@
           <v-row>
             <v-col cols="12" sm="3">
               <div class="white--text font-weight-bold">Company</div>
-              <ul class="d-flex flex-col pa-0 mt-5">
+              <ul class="d-flex flex-column pa-0 mt-5">
                 <li class="mb-2">
                   <router-link class="white--text" to="/">About Us</router-link>
                 </li>
@@ -17,7 +17,7 @@
             </v-col>
             <v-col cols="12" sm="3">
               <div class="white--text font-weight-bold">Top Categories</div>
-              <ul class="d-flex flex-col pa-0 mt-5">
+              <ul class="d-flex flex-column pa-0 mt-5">
                 <li class="mb-2" v-for="category in categories" :key="category.id">
                   <router-link class="white--text" to="/">{{category.name}}</router-link>
                 </li>
@@ -25,12 +25,12 @@
             </v-col>
             <v-col cols="12" sm="3">
               <div class="white--text font-weight-bold">Customer Care</div>
-              <ul class="d-flex flex-col pa-0 mt-5">
+              <ul class="d-flex flex-column pa-0 mt-5">
                 <li class="mb-2">
                   <router-link class="white--text" to="/">Contact Us</router-link>
                 </li>
                 <li class="mb-2">
-                  <router-link class="white--text" to="/">Terms and Conditions</router-link>
+                  <router-link class="white--text" to="/">Terrms and Conditions</router-link>
                 </li>
                 <li class="mb-2">
                   <router-link class="white--text" to="/">Privacy</router-link>
@@ -50,9 +50,9 @@
             </v-col>
           </v-row>
         </v-col>
-        <v-col cols="12" sm="3" class="d-flex flex-col white--text text-center">
-          <h2 class="text-3xl font-weight-black">ASLISEMUA</h2>
-          <div class="mx-auto mt-5">
+        <v-col cols="12" sm="3" class="d-flex flex-column white--text text-center">
+          <h2 class="text-3xl font-weight-bold">ASLISEMUA</h2>
+          <div class="mx-auto d-flex">
             <v-icon color="white">mdi-facebook</v-icon>
             <v-icon color="white">mdi-instagram</v-icon>
             <v-icon color="white">mdi-linkedin</v-icon>
